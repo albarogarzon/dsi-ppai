0435372281b5569b22bcb4bd76d4401a53cc3bca
Darío <43283876+dario97@users.noreply.github.com>
2019-11-13 01:10:55 -0300

    Ahora muestra datos... pero no muestra el tipo de siniestro correctamente, calcula mal el promedio de duración, las fechas las muestra con horas y minutos, y la grilla no muestra nada.

--- implementacionPPAI/implementacionPPAI/entidades/Intervencion.cs
+++ implementacionPPAI/implementacionPPAI/entidades/Intervencion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace implementacionPPAI.Entidades
 {
@@ -49,6 +50,7 @@ namespace implementacionPPAI.Entidades
             }
                     
             duracion = fechaHoraHasta.Subtract(fechaHoraDesde);
+            MessageBox.Show(Convert.ToString(duracion));
 
             return duracion;
         }
